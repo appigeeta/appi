d29839b6ebe1bedae048ff8768d11bdeb7fff6d1
appigeeta <appigeeta@gmail.com>
2014-12-07 11:50:17 -0800

    readcsvfile

---
+++ readCsvFile.py
@@ -0,0 +1,25 @@
+# using list finding element in yahoo login
+
+fh=open ("filename.csv",'r')
+
+for i in fh:
+	newi=i.rstrip()
+	data=newi.split(',')
+	print data[0]
+	print data[1]
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+	
+
+
+
+
+
